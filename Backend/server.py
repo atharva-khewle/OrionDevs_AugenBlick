2c36b46cc05b90a2527dbdba9ae8e97a0c7d1115
Atharva <atharvamainwork@gmail.com>
2024-03-18 15:26:37 +0530

    Frontend initialised

--- Backend/server.py
+++ Backend/server.py
@@ -24,39 +24,42 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
+garbage_objects = [898,899,923,999,419,455,463,473,519,653,569,968,700,728,737,783,790,806,868,879,901,907,910,917]
 
 @app.route('/imgupload', methods=['POST'])
 def sendback():
-
     try:
+        print("out")
         if 'file' in request.files:
+            print("in")
             f = request.files['file']
             basepath = os.path.dirname(__file__)
-            filepath = os.path.join(basepath, 'uploads', f.filename)
+            filepath = os.path.join(basepath, 'uploads', secure_filename(f.filename))  
             print("upload folder is", filepath)
             f.save(filepath)
 
             global imgpath
-            predict_img.imgpath = f.filename
-            print("printing predict", predict_img)
+            imgpath = f.filename
+            print("printing predict", imgpath)
 
             file_extension = f.filename.rsplit(".", 1)[-1].lower()
-            if file_extension:
+            if file_extension == 'jpg':
                 img = cv2.imread(filepath)
                 frame = cv2.imencode(".jpg", cv2.UMat(img))[1]
                 image = Image.open(io.BytesIO(frame))
 
                 # Perform the detection
-                yolo = YOLO('best.pt')
+                yolo = YOLO('yolov8n-cls.pt')
                 detections = yolo.predict(image, save=True)
-
-        return jsonify({'text': 'hello'})
+                top5_labels = detections[0].probs.top5
+                count = sum(label in garbage_objects for label in top5_labels)
+                return jsonify({'count': count})
 
     except Exception as e:
-        return jsonify({'text': str(e)}), 500
-    
-    
+        return jsonify({'error': str(e)}), 500
 
+    # Default response if file upload failed
+    return jsonify({'error': 'File upload failed'}), 400
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
